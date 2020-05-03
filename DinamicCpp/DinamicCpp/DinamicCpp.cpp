380f3ac443186c88dcf0e9f47019d20eeb876817
Dmitriy Strahov <strahov@fittin.ru>
2020-05-03 19:45:15 +0300

    Внесено описание структуры Single_List

--- DinamicCpp/DinamicCpp/DinamicCpp.cpp
+++ DinamicCpp/DinamicCpp/DinamicCpp.cpp
@@ -3,7 +3,56 @@
 
 #include <iostream>
 
+using namespace std;
+
+//структура данных
+struct Single_List
+{
+	int Data; //информационное поле
+	Single_List* Next; //адресное поле
+};
+//. . . . . . . . . .
+//указатель на первый элемент списка
+Single_List* Head;
+//. . . . . . . . . .
+//указатель на текущий элемент списка (при необходимости)
+Single_List* Current;
+
+//создание однонаправленного списка (добавления в конец)
+void Make_Single_List(int n, Single_List** Head) {
+	if (n > 0) {
+		(*Head) = new Single_List();
+		//выделяем память под новый элемент
+		cout << "Введите значение ";
+		cin >> (*Head)->Data;
+		//вводим значение информационного поля
+		(*Head)->Next = NULL;//обнуление адресного поля
+		Make_Single_List(n - 1, &((*Head)->Next));
+	}
+}
+
+//Печать однонаправленного списка
+void Print_Single_List(Single_List* Head) {
+	if (Head != NULL) {
+		cout << Head->Data << "\t";
+		Print_Single_List(Head->Next);
+		//переход к следующему элементу
+	}
+	else cout << "\n";
+}
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	Head = new Single_List();
+	Head->Data = 0;
+	Make_Single_List(10, &Head->Next);
+
+	Current = Head;
+	int i = 0;
+	cout << "Вывод списка:" << endl;
+	Print_Single_List(Head);
 }
+
