18e759b9ae359d976e01c2cbb05333c0198be0ce
Дмитрий Страхов <dimka_strahov@outlook.com>
2020-05-13 20:31:25 +0300

    Double list - начало ^_^

--- DinamicCpp/DinamicCpp/DinamicCpp.cpp
+++ DinamicCpp/DinamicCpp/DinamicCpp.cpp
@@ -3,6 +3,21 @@
 
 #include <iostream>
 
+struct Double_List //структура данных
+{
+	int Data; 			//информационное поле
+	Double_List* Next, 	//адресное поле
+		* Prior;	//адресное поле
+};
+
+//указатель на первый элемент списка
+
+Double_List* Head;
+
+//указатель на текущий элемент списка (при необходимости)
+Double_List* Current;
+
+
 int main()
 {
     std::cout << "Hello World!\n";
